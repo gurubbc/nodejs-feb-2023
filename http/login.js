bafc2094f8723fbcfb7884942a716b0042f958cb
gurubbc <gurumurthy.ramamurthy@gmail.com>
2023-02-17 18:33:52 +0530

    Implemented the max attempt count

--- http/login.js
+++ http/login.js
@@ -1,8 +1,18 @@
 // Reading Query String from the request
 var http = require('http')
 var url = require('url')
-
+var cnt=0
 http.createServer((req, res) => {
+
+    
+
+    if (cnt >= 5)
+    {
+        
+        res.write("<body bgcolor='red'>")
+        res.write("You have exceeded the max attempt...start the application....")
+        return
+    }
     var urlParts = url.parse(req.url, true) // want to access query object
     //    console.log(urlParts)
     var query = urlParts.query // query object
@@ -14,8 +24,11 @@ http.createServer((req, res) => {
         res.write("<body bgcolor='green'>")
         res.write("Welcome..." + myName + " you are a valid user")
     } else {
+        cnt++;
+        console.log("Wrong Attempt# ",cnt)
         res.write("<body bgcolor='red'>")
         res.write("You are an invalid user...please try login again")
+        res.write("<a href='http://127.0.0.1:5502/http/Login.html'>Login again </>")
     }
 
    
